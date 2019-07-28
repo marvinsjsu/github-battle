bcd30bf0a3040fbad6cfffc84a35611f040296cd
Marvin Mante <marvinsjsu@gmail.com>
2019-07-27 21:48:49 -0700

    initial commit; long-overdue

---
+++ app/components/withHover.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default function withHover (Component, hoverKey = 'hovering') {
+  return class WithHover extends React.Component {
+    constructor (props) {
+      super(props);
+
+      this.state = {
+        hovering: false
+      }
+
+      this.handleMouseOver = this.handleMouseOver.bind(this);
+      this.handleMouseOut = this.handleMouseOut.bind(this);
+    }
+
+    handleMouseOver() {
+      this.setState({
+        hovering: true
+      });
+    }
+
+    handleMouseOut() {
+      this.setState({
+        hovering: false
+      });
+    }
+
+    render () {
+
+      const props = {
+        [hoverKey]: this.state.hovering,
+        ...this.props
+      };
+
+      return (
+        <div
+          onMouseOver={this.handleMouseOver}
+          onMouseOut={this.handleMouseOut}
+        >
+          <Component {...props} />
+        </div>
+      );
+    }
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Popular from './components/Popular';
+import Battle from './components/Battle';
+import { ThemeProvider } from './contexts/theme';
+import Nav from './components/Nav';
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }));
+      }
+    };
+  }
+
+  render () {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className='container'>
+            <Nav />
+            <Popular />
+            <Battle />
+          </div>
+        </div>
+      </ThemeProvider>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />, // React element
+  document.getElementById('app')// where to render the element to
+);
